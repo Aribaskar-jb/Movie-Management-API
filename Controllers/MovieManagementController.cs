6ca2c2baaf5e373040078e686ebe6d6b8ab2e762
Aribaskar-jb <j.b.aribaskar1515@gmail.com>
2024-01-28 23:12:12 +0530

    Add project files.

---
+++ Controllers/MovieManagementController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Movie_Management_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieManagementController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetMovies()
+        {
+            var data = new List<Movie>();
+            string jsonContent = System.IO.File.ReadAllText("C:\\Users\\jbari\\Documents\\Kovai.co\\Movie Management API\\Data\\Move.json");
+
+            data = JsonConvert.DeserializeObject<List<Movie>>(jsonContent);
+
+            return Ok(data);
+        }
+        
+        [HttpPost]
+        public IActionResult AddMovie( Movie newMovie)
+        {
+            if (newMovie == null)
+            {
+                return BadRequest("Invalid movie data.");
+            }
+
+            List<Movie> movies = LoadMovies();
+            movies.Add(newMovie);
+            SaveMovies(movies);
+
+            return Ok("Added");
+        }
+        private List<Movie> LoadMovies()
+        {
+            List<Movie> movies;
+            try
+            {
+                string jsonData = System.IO.File.ReadAllText("C:\\Users\\jbari\\Documents\\Kovai.co\\Movie Management API\\Data\\Move.json");
+                movies = JsonConvert.DeserializeObject<List<Movie>>(jsonData);
+            }
+            catch (Exception)
+            {
+                movies = new List<Movie>();
+            }
+            return movies;
+        }
+        private void SaveMovies(List<Movie> movies)
+        {
+            string jsonData = JsonConvert.SerializeObject(movies, Formatting.Indented);
+            System.IO.File.WriteAllText("C:\\Users\\jbari\\Documents\\Kovai.co\\Movie Management API\\Data\\Move.json", jsonData);
+        }
+        [HttpPut]
+        public IActionResult UpdateMovie(Movie Update)
+        {
+            if (Update == null)
+            {
+                return BadRequest("Invalid movie data.");
+            }
+            string title = Update.title;
+            List<Movie> movies = LoadMovies();
+            foreach (Movie movie in movies)
+            {
+                if (movie.title == title)
+                {
+                    movie.title = title;
+                    movie.director = Update.director;
+                    movie.genre = Update.genre;
+                    movie.year = Update.year;
+                }
+            }
+            SaveMovies(movies);
+            return Ok("Updated Movie");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteMovie(string Title)
+        {
+            if (Title == null)
+            {
+                return BadRequest("Invalid movie data.");
+            }
+            List<Movie> movies = LoadMovies();
+            int Count = 0;
+            bool temp = false;
+            foreach (Movie movie in movies)
+            {
+                if(movie.title == Title)
+                {
+                    temp= true;
+                    break;
+                }
+                Count++;
+            }
+            if (temp)
+            {
+                movies.RemoveAt(Count);
+                SaveMovies(movies);
+                return Ok("Delete Movie");
+                
+            }
+            else
+            {
+                return Ok("Enter the valid Movie");
+            }
+        }
+    }
+}
---
+++ MovieManagement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Movie_Management_API
+{
+    public class Movie()
+    {
+        public string title { get; set; }
+        public string genre { get; set; }
+        public int year { get; set; }
+        public string director { get; set; }
+    }
+    public class MovieManagement
+    {
+       
+        private List<Movie> movies = new List<Movie>
+        {
+            new Movie
+            {
+                title = "The Shawshank Redemption",
+                genre = "Drama",
+                year = 1994,
+                director = "Frank Darabont"
+            }
+        };
+        public List<Movie> GetMovieData()
+        {
+            return movies;
+        }
+        public List<Movie> AddMovie(Movie movie)
+        {
+            movies.Add(movie);
+            return movies;
+        }
+
+    }
+}
